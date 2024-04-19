7b7b75180930fc3547d36672ec3f48e1aed873cd
jdawson25 <john.dawson25@trinityschoolnyc.org>
2024-04-19 11:39:53 -0400

    MS1

---
+++ Data Journalism Project/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_page='index')
+
+@app.route('/micro')
+def micro():
+    
+    
+    return render_template('micro.html', current_page='micro', )
+
+@app.route('/about')
+def about():
+    return render_template('about.html', current_page='about')
+
+app.run(debug=True)
